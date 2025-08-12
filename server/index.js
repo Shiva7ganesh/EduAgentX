6d225af0975405905bc1e049aab1ff7c1fa80a58
Shiva7ganesh <22r01a0531@cmrithyderabad.edu.in>
2025-08-12 12:27:24 +0530

    added vercel to cors

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: ["http://localhost:5173", "http://localhost:3000", "https://edu-automators.vercel.app"],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   })
