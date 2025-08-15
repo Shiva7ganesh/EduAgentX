4186aac3e8622ac2d23b4b3d3aa34f77d3f3c7e1
Vardhan Ganugula <gvardhan2004@gmail.com>
2025-08-15 21:02:31 +0530

    add github webhook

---
+++ server/components/github.component.js
@@ -0,0 +1,28 @@
+import { exec } from "child_process";
+import crypto from "crypto";
+
+function verifySignature(req) {
+    const SECRET = process.env.GITHUB_WEBHOOK_PASS;
+    const signature = req.headers["x-hub-signature-256"];
+    const hmac = crypto.createHmac("sha256", SECRET);
+    const digest =
+        "sha256=" + hmac.update(JSON.stringify(req.body)).digest("hex");
+    return signature === digest;
+}
+
+export const githubWebHook = (req, res) => {
+  if (!verifySignature(req)) {
+    return res.status(401).send("Invalid signature");
+  }
+
+  console.log("Push event received. Running update.sh...");
+
+  exec("bash ../../update.sh", (err, stdout, stderr) => {
+    if (err) {
+      console.error(stderr);
+      return res.status(500).send("Update failed");
+    }
+    console.log(stdout);
+    res.status(200).send("Update completed");
+  });
+};
